bcb5496a55cf3c5352c57bae3887d77a763e3039
Amjath s <amjaths788@gmail.com>
2025-06-16 21:59:07 +0530

    5 patterns

---
+++ Beginner.py
@@ -0,0 +1,116 @@
+
+def SolidSquare():
+
+    no_of_start=int(input("enter the row required for rectangle "))
+    for i in range(1,no_of_start+1):
+
+
+        for j in range(1,no_of_start+1):
+
+            print('*',end="")
+        print("")
+    return 
+
+def HollowSquare():
+    no_of_rows=int(input("enter the row required for rectangle "))
+    for i in range(1,no_of_rows+1):
+        if i==1 or i==no_of_rows:
+            for j in range(1,no_of_rows+1):
+                print('* ',end="")
+        else:
+            for j in range(1,no_of_rows+1):
+                if j==1 or j==no_of_rows:
+                    print("*",end="")
+                else:
+                    print("  ",end="")
+        print("")      
+        print("")
+    return 
+
+def LeftangledTraingle():
+    no_of_rows=int(input("enter the no of rows"))
+    for i in range(1,no_of_rows+1):
+        for j in range(1,i+1):
+            print("*",end="")
+        print("")
+def HollowLeft():
+    no_of_rows=int(input("enter your rows"))
+    for i in range(1,no_of_rows+1):
+        if i < no_of_rows:
+            for j in range(1,i+1):
+                if j==1 or j==i:
+                    print("*",end="")
+                else:
+                    print(" ",end="")
+            print("")
+        else:
+            for j in range(1,no_of_rows+1):
+                print("*",end="")
+            print("")
+
+def RightalignedTriangle():
+    no_of_rows=int(input("enter the no of rows"))
+    for i in range(1,no_of_rows+1):
+        for j in range(1,no_of_rows-i+1):
+            print(" ",end="")
+        
+        if i==1 or i==no_of_rows:
+            for k in range(1,i+1):
+               
+                print("*",end="")
+               
+        else:
+            for k in range(1,no_of_rows+1):
+                if k==1 or k==i:
+
+                    print("*",end="")
+                else:
+                    print(" ",end="")
+        print("")
+        
+
+
+          
+
+          
+    
+
+def Printpattern():
+
+    Options='yes'
+    while Options !="no":
+
+        
+        print("---Choice---")
+        print("1:for Solid Sqaure \n" 
+        "2:for Hollow square\n"
+        "3:for left alinged  right angled trianlge\n"
+        "4:for hollow aligned right angled triangle\n")
+        choice=int(input("Enter your choice "))
+
+        if choice==1:
+             
+            SolidSquare()
+        elif choice==2:
+            HollowSquare()
+        elif choice==3:
+            LeftangledTraingle()
+        elif choice==4:
+            HollowLeft()
+        elif choice==5:
+            RightalignedTriangle()
+       
+
+
+
+           
+        
+        else:
+            print("invalid Choice")
+        
+        Options=input("Do you want to continue yes/no")
+
+
+    
+if __name__ == "__main__":
+    Printpattern()
